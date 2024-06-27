4bd911fe2f265c260d1738fbffc319f5126c7ee4
EMMANUEL <waruterewachira7@gmail.com>
2024-06-28 01:07:13 +0300

    complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    """Returns a list of even numbers from a list of integers."""
+
+    return [num_list[i] for i in range(len(num_list)) if num_list[i] % 2 == 0]
+
+'''The function uses a list comprehension to iterate over the indices of the input list (range(len(num_list))) 
+and checks if the number at the current index is even (num_list[i] % 2 == 0).'''
+
 
 def make_exclamation(sentence_list):
-    pass
+    """Returns a list of sentences with exclamation marks."""
+
+    return [sentence_list[i] + "!" for i in range(len(sentence_list))]
+
+'''The function uses a list comprehension to iterate over the indices of the input list (range(len(sentence_list))) and creates a new list by concatenating each sentence with an exclamation mark (sentence_list[i] + "!"). 
+    The resulting list is then returned by the function'''
+    
